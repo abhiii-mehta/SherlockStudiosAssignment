e6170362748e7f40b55b473bc41a41cd6b289b58
Abhinandan <abhimehta1324@gmail.com>
2025-08-04 12:46:09 +0200

    movement set up

---
+++ Assets/Scripts/FPSControllerScript.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float lookSensitivity = 2f;
+    public float jumpHeight = 1.5f;
+    public float gravity = -9.81f;
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private float verticalVelocity;
+    private float xRotation = 0f;
+
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+    private InputAction toggleFlashlightAction;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+
+        var playerMap = playerInput.actions.FindActionMap("Player", throwIfNotFound: true);
+        moveAction = playerMap.FindAction("Move", throwIfNotFound: true);
+        lookAction = playerMap.FindAction("Look", throwIfNotFound: true);
+        jumpAction = playerMap.FindAction("Jump", throwIfNotFound: true);
+        toggleFlashlightAction = playerMap.FindAction("ToggleLight", throwIfNotFound: false);
+    }
+
+    void OnEnable()
+    {
+        moveAction.Enable();
+        lookAction.Enable();
+        jumpAction.Enable();
+        toggleFlashlightAction?.Enable();
+    }
+
+    void OnDisable()
+    {
+        moveAction.Disable();
+        lookAction.Disable();
+        jumpAction.Disable();
+        toggleFlashlightAction?.Disable();
+    }
+
+    void Update()
+    {
+        HandleLook();
+        HandleMovement();
+
+        bool isGrounded = controller.isGrounded || Physics.Raycast(transform.position, Vector3.down, 1.1f);
+
+        if (isGrounded && jumpAction.WasPressedThisFrame())
+        {
+            Debug.Log("Jump pressed!");
+            verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+        controller.Move(Vector3.up * verticalVelocity * Time.deltaTime);
+    }
+
+    private void HandleMovement()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+
+    private void HandleLook()
+    {
+        lookInput = lookAction.ReadValue<Vector2>();
+        xRotation -= lookInput.y * lookSensitivity * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * lookInput.x * lookSensitivity * Time.deltaTime);
+    }
+}
